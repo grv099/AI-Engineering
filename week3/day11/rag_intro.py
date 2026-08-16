500c57e5d2a4d718c4596072899df30d9131d671
grv099 <dubeygaurav3899@gmail.com>
2026-08-16 23:47:39 +0530

    Refactor code structure for improved readability and maintainability

---
+++ week3/day11/rag_intro.py
@@ -0,0 +1,54 @@
+import os
+from pathlib import Path
+from time import sleep
+from dotenv import load_dotenv
+from groq import Groq
+import re
+
+load_dotenv()
+
+my_api_key = os.getenv("GROQ_API_KEY")
+
+if not my_api_key:
+    raise ValueError("GROQ_API_KEY environment variable is not set.")
+
+client = Groq(api_key=my_api_key)
+
+my_model = "llama-3.3-70b-versatile"
+
+# step 1
+knowledge_base={
+    "age" : " The age of pratyush is 25 years",
+    "net worth" : "The net worth of pratyush is 2000"
+}
+
+# step 2 retreieval
+def retrieve_info(question):
+    question=question.lower()
+    if "age" in question:
+        return knowledge_base["age"]
+    elif "net worth" in question:
+        return knowledge_base["net worth"]
+    else:
+        return None
+def ask_llm(question):
+    context=retrieve_info(question)
+
+    sys_prompt=f"""answer in one line only. Answer only based on this context. do not hallucinate. Context: {context}"""
+    system_message={
+        "role": "system",
+        "content": sys_prompt
+
+    }
+    message={
+        "role": "user",
+        "content": question
+    }
+    messages=[system_message, message]
+    response=client.chat.completions.create(model=my_model, messages=messages)
+    answer=response.choices[0].message.content
+    return answer
+
+
+question="what is pratyush's age?"
+print(ask_llm(question))
